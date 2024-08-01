720655dc2940edaa0291cb62dfab39c6f309d624
GokilavaniSenthilnathan <146613829+GokilavaniSenthilnathan@users.noreply.github.com>
2024-08-02 00:21:45 +0530

    Add files via upload

---
+++ CrudSpringProApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_Spring_Pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringProApplication.class, args);
+	}
+
+}
